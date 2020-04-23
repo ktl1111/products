d3ba360a6028b8d67b5837139bea9f89430423ee
Vany Liu <vany8462@gmail.com>
2020-04-23 15:34:15 +0800

    add header

--- products.py
+++ products.py
@@ -5,7 +5,9 @@ while True:
 	if name == 'q':
 		break
 	price = input('Enter price: ')
+	price = int(price)
 	stock = input('Enter number of stock: ')
+	stock = int(stock)
 	# p = []
 	# p.append(name)
 	# p.append(price)
@@ -19,3 +21,9 @@ print(products[0][2])
 
 for product in products:
 	print(product[0], 'price: NT', product[1], 'remaining stock: ', product[2])
+
+#write file
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('Product Name, Price(NTD), Stock\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + ',' + str(p[2]) + '\n')
